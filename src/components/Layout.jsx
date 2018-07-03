0716759ea8c0abeed74a8e7a72ae2c5002ce621f
Salvador Jimenez Mendoza <salvador@michelada.io>
2018-07-03 15:10:11 -0500

    implementing a random function ver. 2.0

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,7 +11,7 @@ class Layout extends Component {
           image: ImagesList[Math.floor(Math.random() * 3)],
         }
       )),
-    randomString: randomString('blue'),
+    randomString: randomString('B'),
   };
 
   handleRevealCard = (key) => {
--- src/utils/randomString.jsx
+++ src/utils/randomString.jsx
@@ -1,43 +1,7 @@
 const randomString = startingTeam => {
-  const teamCardsNumber = 7;
-
-  const colorCards = {
-    blue: startingTeam === 'blue' ? teamCardsNumber + 1 : teamCardsNumber,
-    red:  startingTeam === 'red' ? teamCardsNumber + 1 : teamCardsNumber,
-    green: 4,
-    dark: 1
-  };
-
-  const colorValue = (value) => {
-    switch(value){
-      case 0:
-        return 'B';
-        break;
-      case 1:
-        return 'R';
-        break;
-      case 2:
-        return 'G';
-        break;
-      case 3:
-        return 'D';
-        break;
-    }
-  };
-
-  let cardsCounter = 20
-  const colorCardsKeys = Object.keys(colorCards);
-  let colorsString = '';
-
-  while(cardsCounter > 0){
-    let random = Math.floor(Math.random() * 4);
-    if (colorCards[colorCardsKeys[random]] > 0){
-      cardsCounter--;
-      colorCards[colorCardsKeys[random]]--;
-      colorsString += colorValue(random);
-    }
-  }
-
+  const colorsString = `BBBBBBBRRRRRRRDPPPP${startingTeam}`
+                       .split('')
+                       .sort(() => 0.5 - Math.random() ).join('');
   return colorsString;
 }
 
