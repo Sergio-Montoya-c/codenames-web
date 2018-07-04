6352b14f0d216f3df1fce24f603af5f10de2c899
Salvador Jimenez Mendoza <salvador@michelada.io>
2018-07-04 12:50:14 -0500

    Refactor generateRandomImages function

--- src/utils/generateCardsArray.jsx
+++ src/utils/generateCardsArray.jsx
@@ -2,16 +2,30 @@ import ImagesList from '../components/Card/images_list';
 
 const generateCardsArray = randomString => {
   const fieldArray = randomString.split("");
-  const fields = fieldArray.map(field => ({
+
+  const generateRandomImages = () => {
+    let images =  [...ImagesList]
+    let randomImages = [];
+    for (let index = 0; index < 20; index++) {
+      let randomNumber = Math.floor(Math.random() * images.length);
+      randomImages.push(...images.splice(randomNumber, 1));
+    }
+    return randomImages;
+  };
+
+  const randomImages = generateRandomImages();
+
+  const fields = fieldArray.map(((field, index) => ({
       selected: false,
       agent: field,
-      image: ImagesList[Math.floor(Math.random() * 3)],
+      image: randomImages[index],
     })
-  );
+  ));
+
   return {
     cardsArray: fields,
     randomString: randomString,
-  }
+  };
 };
 
 export default generateCardsArray;
